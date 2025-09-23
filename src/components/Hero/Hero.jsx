d745a56a926e63e9a08134234af53a5673fbe39c
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-09-23 13:29:10 +0100

    Complete hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import chiemeziePicture from "../../assets/chiemezie-uche.png";
+import { FaDownload } from "react-icons/fa6";
 
 const Hero = () => {
     return (
@@ -12,26 +13,26 @@ const Hero = () => {
             </div>
 
             <h2 
-                className="text-white-shade capitalize font-extrabold text-2xl sm:text-3xl lg:text-5xl"
+                className="text-white-shade capitalize font-extrabold text-2xl sm:text-3xl lg:text-5xl tracking-wide"
             >
                 Chiemezie Uchenwoke
             </h2>
 
-            <p className="text-lite-gray w-[90%] max-w-[47rem] text-center text-sm sm:text-base lg:text-[1.25rem] leading-6 sm:leading-7 lg:leading-8 font-medium">
+            <p className="text-lite-gray w-[90%] max-w-[39rem] lg:max-w-[47rem] text-center text-sm sm:text-base lg:text-[1.25rem] leading-6 sm:leading-7 lg:leading-8 font-medium">
                 Fullstack Developer skilled in building modern & responsive web solutions that delivers value and enhance user experience. I help businesses and organizations bring their ideas to life through innovative design and development.
             </p>
 
-            <div className="px-4 w-full flex flex-col min-[430px]:flex-row min-[430px]:justify-center gap-4">
+            <div className="px-4 w-full flex flex-col min-[450px]:flex-row min-[450px]:justify-center gap-4 sm:gap-6">
                 <a 
                     href="#"
-                    className="bg-pri-blue text-white-shade py-3 lg:py-3.5 px-10 rounded-md font-semibold capitalize inline-block w-full text-center min-[430px]:w-fit hover:scale-105 duration-300 transition ease-in-out hover:bg-blue-600/90"
+                    className="bg-pri-blue text-white-shade py-3 lg:py-3.5 px-10 rounded-md font-semibold capitalize flex gap-2 items-center justify-center w-full text-center min-[450px]:w-fit hover:scale-105 duration-300 transition ease-in-out hover:bg-blue-600/90"
                 >
-                    my resume
+                    <FaDownload className="animate-bounce" /> Download Cv
                 </a>
 
                 <a 
                     href="#"
-                    className="border border-pri-blue text-pri-blue rounded-md font-semibold capitalize inline-block w-full text-center py-3 lg:py-3.5 px-10 min-[430px]:w-fit transition hover:bg-pri-blue hover:text-white-shade duration-300"
+                    className="border border-pri-blue text-pri-blue rounded-md font-semibold capitalize inline-block w-full text-center py-3 lg:py-3.5 px-10 min-[450px]:w-fit transition hover:bg-pri-blue hover:text-white-shade duration-300"
                 >
                     view my work
                 </a>
