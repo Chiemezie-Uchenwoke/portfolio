7b1d5018fb7a243419701d78f6e216cc2d3823be
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-09-23 10:47:29 +0100

    change background to dark and create a container class to control width

--- src/components/Header/HeaderLg.jsx
+++ src/components/Header/HeaderLg.jsx
@@ -1,7 +1,23 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+
 const HeaderLg = () => {
     return (
-        <header>
-            
+        <header className="">
+            <div>
+                <h1>Chiemezie</h1>
+
+                <nav>
+                    <a href="#">projects</a>
+                    <a href="#">contact</a>
+                </nav>
+
+                <div>
+                    <a href="#"> <FaGithub /> </a>
+                    <a href="#"> <FaLinkedin /> </a>
+                    <a href="#"> <IoMdMail /> </a>
+                </div>
+            </div>
         </header>
     )
 }
