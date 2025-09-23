4976b410fc4a4f2fae6349fdc1678c4003825d71
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-09-23 13:59:23 +0100

    Add project section heading and intro

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,13 @@
 const Projects = () => {
     return (
-        <section>
-            
+        <section className="container mt-16 flex flex-col items-center">
+            <div className="flex flex-col items-center gap-3">
+                <h2 className="text-white-shade capitalize font-bold text-xl">featured projects</h2>
+
+                <p className="text-lite-gray text-sm sm:text-base lg:text-lg text-center">
+                    A showcase of my recent work and creative solutions
+                </p>
+            </div>
         </section>
     )
 }
