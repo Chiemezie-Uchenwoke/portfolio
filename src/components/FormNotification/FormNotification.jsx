56a5ad317c61a58ea0f4a7d9f65a8a3e6db97289
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-09-26 10:53:54 +0100

    Change formNotification modal style

--- src/components/FormNotification/FormNotification.jsx
+++ src/components/FormNotification/FormNotification.jsx
@@ -3,10 +3,10 @@ import { IoMdClose } from "react-icons/io";
 const FormNotification = ({title, message, onClose, isSuccess, isError}) => {
     return (
         <div className="fixed top-0 left-0 z-[70] bg-dark-bg/60 flex justify-center items-center w-full h-full">
-            <div className={`bg-lite-gray w-[90%] max-w-100 relative z-[75] rounded-lg py-8 px-4 border ${isSuccess ? "border-dark-bg" : "border-black/30"} ${isError ? "border-red-400" : "border-black/30"} shadow-lg flex flex-col gap-4`}>
+            <div className={`bg-main-dark-bg w-[90%] max-w-100 relative z-[75] rounded-lg py-8 px-4 border shadow-lg flex flex-col gap-4`}>
                
                 <button 
-                    className="border border-black/30 w-8 h-8 rounded-full flex justify-center items-center absolute right-4 top-3 cursor-pointer hover:bg-pri-blue hover:text-white-shade"
+                    className="border border-white/20 hover:border-white/40 w-8 h-8 rounded-full flex justify-center items-center absolute right-4 top-3 cursor-pointer text-white-shade/40 hover:text-white-shade/70 duration-200"
                     onClick={onClose}
                 >
                     <IoMdClose className="text-lg" />
@@ -14,7 +14,7 @@ const FormNotification = ({title, message, onClose, isSuccess, isError}) => {
 
                 <h2 className={`capitalize font-semibold ${isSuccess ? "text-pri-blue" : ""} ${isError ? "text-red-400" : ""}`}> {title} </h2>
 
-                <p className="text-sm">
+                <p className="text-sm text-white-shade/60">
                     {message}
                 </p>
             </div>
