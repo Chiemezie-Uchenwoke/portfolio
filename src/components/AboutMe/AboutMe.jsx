ac43ece40510425a9f48c13c034651d9e2ece77a
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-09-26 14:50:02 +0100

    Complete about page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,64 +3,59 @@ import { CiUser } from "react-icons/ci";
 
 const AboutMe = () => {
     return (
-        <div className="h-[calc(100vh-4rem)] ">
-            <div 
-                className="container flex flex-col md:flex-row gap-8 mt-8"
-            >
-                <div className="border border-white-shade/15 rounded-lg w-full md:w-[20rem] flex flex-col gap-3.5 py-6 px-4">
-                    <div className="w-48 h-48 rounded-full overflow-hidden bg-dark-gray/10">
-                        <img 
-                            src={myPic} 
-                            alt="Chiemezie Uchenwoke's picture" 
-                            className="w-full h-full object-cover relative top-4"
-                        />
-                    </div>
+        <div 
+            className="container flex flex-col md:flex-row gap-8 mt-8"
+        >
+            <div className="border border-white-shade/15 rounded-lg w-full md:w-[17rem] flex flex-col gap-3.5 py-6 px-4 h-auto">
+                <div className="w-48 h-48 rounded-full overflow-hidden bg-dark-gray/10">
+                    <img 
+                        src={myPic} 
+                        alt="Chiemezie Uchenwoke's picture" 
+                        className="w-full h-full object-cover relative top-4"
+                    />
+                </div>
 
-                    <h4 className="text-pri-blue/90 flex items-center gap-2 self-start">
-                        <CiUser /> Chiemezie Uchenwoke
-                    </h4>
+                <h4 className="text-pri-blue/90 flex items-center gap-2 self-start">
+                    <CiUser /> Chiemezie Uchenwoke
+                </h4>
 
-                    <p className="text-white-shade/60 text-sm">
-                        I specialize in creating modern, responsive applications that deliver exceptional user experiences.
+                   <p className="text-white-shade/60 text-sm">
+                       I specialize in creating modern, responsive applications that deliver exceptional user experiences.
+                   </p>
+
+                <a 
+                    href="mailto:uchenwoke.chiemezie@gmail.com?subject=Project%20Inquiry" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white-shade bg-pri-blue inline-block w-full mt-2 py-2 px-10 text-center capitalize rounded-lg hover:bg-pri-blue/80 duration-300"
+                >
+                    get in touch
+                </a>
+            </div>
+
+            <div className="w-full md:w-[calc(100%-17rem)] h-auto overflow-auto flex flex-col gap-6 rounded-lg">
+                <h2 className="capitalize text-white-shade font-bold sm:text-lg lg:text-xl">about me</h2>
+                <div className="text-white-shade/60 text-sm min-[900px]:text-base flex flex-col gap-4">
+                    <p>
+                        I am a Fullstack Developer skilled in building responsive, user-centered web applications using technologies like Node.js, Express, React, and Tailwind CSS. I have a solid foundation in frontend development and a growing expertise in backend systems, RESTful API design, authentication, and database management. My journey has been shaped by a commitment to continuous learning and a drive to create clean, functional, and visually engaging web solutions.
                     </p>
 
-                    <a 
-                        href="mailto:uchenwoke.chiemezie@gmail.com?subject=Project%20Inquiry" 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-white-shade bg-pri-blue inline-block w-full py-2 px-10 text-center capitalize rounded-lg hover:bg-pri-blue/80 duration-300"
-                    >
-                        get in touch
-                    </a>
+                    <p>
+                        Currently, I am expanding my knowledge of backend development by working with PostgreSQL & MongoDB, designing database schemas, writing efficient queries, and connecting APIs securely between client and server. I enjoy applying what I learn directly into projects and solving real world problems. 
+                    </p>
+
+                    <p>
+                        Beyond technical skills, I am deeply passionate about mentorship and knowledge-sharing. I believe in fostering collaborative environments and am committed to continuous growth, both as a developer and a professional.
+                    </p>
                 </div>
 
-                <div className="w-full md:w-[calc(100%-20rem)] h-auto overflow-auto flex flex-col gap-6 border border-white-shade/15 py-6 px-4 rounded-lg">
-                    <h2 className="capitalize text-white-shade font-bold sm:text-lg lg:text-xl">about me</h2>
-
-                    <div className="text-white-shade/60 text-sm min-[900px]:text-base flex flex-col gap-4">
-                        <p>
-                            I am a Fullstack Developer skilled in building responsive, user-centered web applications using technologies like Node.js, Express, React, and Tailwind CSS. I have a solid foundation in frontend development and a growing expertise in backend systems, RESTful API design, authentication, and database management. My journey has been shaped by a commitment to continuous learning and a drive to create clean, functional, and visually engaging web solutions.
-
-
-                        </p>
-
-                        <p>
-                            Currently, I am expanding my knowledge of backend development by working with PostgreSQL & MongoDB, designing database schemas, writing efficient queries, and connecting APIs securely between client and server. I enjoy applying what I learn directly into projects and solving real world problems. 
-                        </p>
-
-                        <p>
-                            Beyond technical skills, I am deeply passionate about mentorship and knowledge-sharing. I believe in fostering collaborative environments and am committed to continuous growth, both as a developer and a professional.
-                        </p>
-                    </div>
-
-                    <a 
-                        className="border border-white-shade/30 bg-white-shade/20 text-white-shade my-3 py-3 rounded-lg font-semibold cursor-pointer hover:bg-white-shade/25 duration-300 active:scale-95 text-center"
-                        href="/Chiemezie_Uchenwoke_CV.pdf"
-                        download
-                    >
-                        Download CV
-                    </a>
-                </div>
+                <a 
+                    className="border border-white-shade/30 bg-white-shade/20 text-white-shade my-3 py-3 rounded-lg font-semibold cursor-pointer hover:bg-white-shade/25 duration-300 active:scale-95 text-center"
+                    href="/Chiemezie_Uchenwoke_CV.pdf"
+                    download
+                >
+                    Download CV
+                </a>
             </div>
         </div>
     )
