79bb2a4f10fac047b78d0a8d18e496850c9d742b
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-09-26 15:23:46 +0100

    Add vercel.json file

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,8 @@
 const NotFound = () => {
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+        <div className="w-full max-w-[35rem] py-8 px-4 mt-10 flex flex-col gap-6">
+            <h1 className="font-bold text-white-shade text-xl sm:text-2xl lg:text-5xl">404 - Page Not Found</h1>
+            <p className="text-white-shade/60">The page you are looking for does not exist.</p>
         </div>
     )
 }
