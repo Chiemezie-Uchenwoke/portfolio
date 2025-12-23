cacae8ed11c6aec51e128872c4045f80561f2d9f
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-12-23 15:35:35 +0100

    Fix: Make projects section height fit properly on ios and remove skills animation

--- src/components/Skills/SkillsCard.jsx
+++ src/components/Skills/SkillsCard.jsx
@@ -1,15 +1,7 @@
-import { motion } from "motion/react"; // eslint-disable-line no-unused-vars
-
 const SkillsCard = ({ icon: Icon, skillName, index }) => {
     return (
-        <motion.div
+        <div
             className="bg-pri-blue/20 hover:bg-pri-blue/35 duration-300 py-6 rounded-lg flex flex-col items-center gap-4 border border-white-shade/10 shadow-xl shadow-white-shade/3"
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ delay: index * 0.1, duration: 0.5 }}
-            viewport={{ once: true }}
-            whileHover={{ scale: 1.04 }}
-            whileTap={{ scale: 0.95 }}
         >
             <span className="w-14 h-14 lg:w-16 lg:h-16 flex justify-center items-center border border-white/30 rounded-lg bg-main-dark-bg">
                 <Icon className="text-white-shade text-3xl lg:text-4xl" />
@@ -18,7 +10,7 @@ const SkillsCard = ({ icon: Icon, skillName, index }) => {
             <h4 className="text-white-shade/85 font-bold">
                 {skillName}
             </h4>
-        </motion.div>
+        </div>
     )
 }
 
